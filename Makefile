365b9dc26a281ad3922411a5998964c557f98e83
Marcin Kościelnicki <koriakin@0x04.net>
2014-06-03 17:18:10 +0200

    a

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ KERNEL_FILES = kernel.c vga.c print.c pic.c thread.c palloc_np.c string.c palloc
 KERNEL_HEADERS = vga.h io.h print.h pic.h main_debug.h thread.h multiboot.h palloc_np.h string.h palloc.h
 
 kernel: $(KERNEL_FILES) kernel.lds $(KERNEL_HEADERS)
-	gcc -m32 -ffreestanding -nostdlib $(KERNEL_FILES) -o kernel -Wl,-T,kernel.lds
+	gcc -Wall -m32 -ffreestanding -nostdlib $(KERNEL_FILES) -o kernel -Wl,-T,kernel.lds
 
 install: kernel
 	sudo mount fd.img fd
--- palloc_np.c
+++ palloc_np.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include "multiboot.h"
 #include "print.h"
+#include "io.h"
 
 extern multiboot_info_t *multiboot_info;
 
@@ -32,7 +33,11 @@ void map_page_np(uint32_t *pd, uint32_t phys, uint32_t virt) {
 	int pdi = virt >> 22 & 0x3ff;
 	int pti = virt >> 12 & 0x3ff;
 	if (!pd[pdi]) {
-		pd[pdi] = palloc_np() | 1;
+		uint32_t *pt = (uint32_t *)palloc_np();
+		pd[pdi] = (uint32_t)pt | 1;
+		int i;
+		for (i = 0; i < 0x400; i++)
+			pt[i] = 0;
 	}
 	uint32_t *pt = (uint32_t *)(pd[pdi] & ~0xfff);
 	pt[pti] = phys | 1;
@@ -96,11 +101,8 @@ void init_palloc() {
 		map_page_np(pd, page, page);
 	}
 	map_page_np(pd, free_list, 0);
-	asm volatile ("movl %0, %%cr3\n" : : "r"(pd));
-	uint32_t cr0;
-	asm volatile ("movl %%cr0, %0\n" : "=r"(cr0):);
-	cr0 |= 1 << 31;
-	asm volatile ("movl %0, %%cr0\n" : : "r"(cr0));
+	set_cr3((uint32_t)pd);
+	set_cr0(get_cr0() | 1 << 31);
 	printf(default_window, "PD %x\n", pd);
 	printf(default_window, "PD PD %x\n", *(uint32_t *)0xfffffffc);
 }
