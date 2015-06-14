209de1034e1b303b520ff9d950afae53a6117cd2
Marcin Kościelnicki <koriakin@0x04.net>
2015-06-14 11:27:22 +0200

    mb.h

---
+++ mb.h
@@ -0,0 +1,29 @@
+#ifndef MB_H
+#define MB_H
+#include <stdint.h>
+
+struct mb_header {
+	uint32_t flags;
+	uint32_t mem_lower;
+	uint32_t mem_upper;
+	uint32_t boot_device;
+	char *cmdline;
+	uint32_t mods_count;
+	void *mods_addr;
+	uint32_t syms[4];
+	uint32_t mmap_length;
+	void *mmap_addr;
+};
+
+#define MB_FLAGS_MMAP	0x00000040
+
+struct mb_mmap {
+	uint32_t size;
+	uint32_t base_lo;
+	uint32_t base_hi;
+	uint32_t length_lo;
+	uint32_t length_hi;
+	uint32_t type;
+};
+
+#endif
