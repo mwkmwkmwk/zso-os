292fc8bbae82c7c43844404e5a0817d394e36259
Bartosz Stebel <bartoszstebel@gmail.com>
2017-09-10 23:34:34 +0200

    fix intel_kvm crash - don't use 1G pages

--- b64.asm
+++ b64.asm
@@ -4,6 +4,8 @@ bits 64
 start:
 
 mov rax, 0xffff8000000b8000
+;times 0x50 nop
+;mov rax, 0x00b8000
 mov rdx, 0x0c340c360c4d0c50
 mov [rax+160], rdx
 
--- pm.asm
+++ pm.asm
@@ -1,6 +1,6 @@
 org 0x90400
 bits 32
-
+;%define HUGEPAGES
 start:
 
 mov eax, 0
@@ -13,6 +13,7 @@ mov dword [0x8e800], 0x8f003
 mov dword [0x8e804], 0
 mov ecx, 0x200
 mov ebx, 0
+%ifdef HUGEPAGES
 mov eax, 0x00000083
 mov edx, 0
 .loop:
@@ -22,6 +23,44 @@ add eax, 0x40000000
 adc edx, 0
 add ebx, 8
 loop .loop
+
+%else
+mov ebx, 0
+mov dword [0x8f000+ebx], 0x8c003
+mov dword [0x8f004+ebx], 0
+add ebx, 24
+mov dword [0x8f000+ebx], 0x8d003
+mov dword [0x8f004+ebx], 0
+
+mov ecx, 0x200
+mov ebx, 0
+mov eax, 0x00000083
+mov edx, 0
+.loop_lower:
+mov dword [0x8c000+ebx], eax
+mov dword [0x8c004+ebx], edx
+add eax, 0x200000
+adc edx, 0
+add ebx, 8
+loop .loop_lower
+
+;this really should be a nested loop
+
+mov ecx, 0x200
+mov ebx, 0
+mov eax, 0x00000083 | 0xc0000000
+mov edx, 0
+.loop_upper:
+mov dword [0x8d000+ebx], eax
+mov dword [0x8d004+ebx], edx
+add eax, 0x200000
+adc edx, 0
+add ebx, 8
+loop .loop_upper
+
+
+%endif
+
 mov eax, 0x8e000
 mov cr3, eax
 mov eax, cr4
@@ -39,8 +78,10 @@ mov cr0, eax
 jmp 0x18:0x90600
 
 
+
 mov eax, 0xb8000
 mov [ds:eax+160], dword 0x0c320c33
 hlt
+
 
 align 0x200
